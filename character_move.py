792d2b3200e0217c02ca5eb904954b42b9643ab4
acientsquirrel <hanbong34@gmail.com>
2025-09-19 21:00:27 +0900

    사각 운동 함수에 필요한 다른 함수 뼈대 만들

--- character_move.py
+++ character_move.py
@@ -6,7 +6,31 @@ open_canvas()
 boy = load_image('character.png')
 
 
+def top_move():
+    pass
+
+
+def right_move():
+    pass
+
+
+def bottom_move():
+    pass
+
+
+def left_move():
+    pass
+
+
 def rectangle_move():
+    clear_canvas()
+    top_move()
+    right_move()
+    bottom_move()
+    left_move()
+
+    boy.draw_now(400, 300)
+    delay(0.1)
     pass
 
 
